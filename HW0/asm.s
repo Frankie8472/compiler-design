8e37aa567306dc26ea278476c12f6894fb6556ca
Josua Cantieni <josuac@vis.ethz.ch>
2018-02-28 17:45:05 +0100

    Cleaned up my version

--- HW0/asm.s
+++ HW0/asm.s
@@ -4,30 +4,35 @@
 #-----------------------------
 .globl main
 
-pattern:
-	.string	"%d"
-
+.section .rodata                    # Preinitialized data
+pattern_scan:
+	.string	"%d"                    # Pattern for scanf
+pattern_print:
+    .string "%d\n"                  # Pattern for printf
+    
+.text
 main:
-	pushl	%ebp                # Init-call
+	pushl	%ebp                    # Init-call
 	movl	%esp, %ebp          
-    subl    $8, %esp            # Allocate Stack for one variable, one pointer to $pattern
+    subl    $8, %esp                # Allocate Stack for one variable, one pointer to $pattern
     
-    movl	$pattern, (%esp)    # Move Pointer to pattern to Stack (first argument)
-    leal	4(%esp), %eax       # Store Pointer to %esp+4 in %eax
-	movl	%eax, 4(%esp)       # Store %eax in Stack+4 (second argument)
+    movl	$pattern_scan, (%esp)   # Move Pointer to pattern to Stack (first argument)
+    leal	4(%esp), %eax           # Store Pointer to %esp+4 in %eax
+	movl	%eax, 4(%esp)           # Store %eax in Stack+4 (second argument)
 
-    call    scanf               # Read number. store in Stack+4
+    call    scanf                   # Read number. store in Stack+4
     
-    testl	%eax, %eax          # Test if Number was found
-    movl    $1, %eax            # Set return code if a error occurs 
-	je	    _main_exit          # If no number was found exit
+    testl	%eax, %eax              # Test if Number was found
+    movl    $1, %eax                # Set return code if a error occurs 
+	je	    .L0                     # If no number was found exit
     
-    incl    4(%esp)             # Add One to read value
-	call	printf              # Print read value
+    incl    4(%esp)                 # Add One to read value
+    movl	$pattern_print, (%esp) 
+	call	printf                  # Print read value
     
-    xorl    %eax, %eax          # Returncode 0
+    xorl    %eax, %eax              # Returncode 0
     
- _main_exit:  
-    leave
+.L0:  
+    leave                           # Cleanup
     ret
     
