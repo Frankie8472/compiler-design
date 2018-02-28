152999ae454edb7eee60460c40029ff5d0d9deb4
Frankie <knobelf@student.ethz.ch>
2018-02-28 16:14:00 +0100

    formatting

--- HW0/assembly.s
+++ HW0/assembly.s
@@ -15,11 +15,14 @@
 ## start of main
 main:
 pushl   %ebp            # create new stack frame
-movl    %esp, %ebp
-movl    %esp, (%esp)
-pushl   $.format
+movl    %esp, %ebp      
+
+subl    $4, %esp        # allocate stack
+movl    %esp, (%esp)    # space for var
+pushl   $.format        # save "%d" on stack
 call    scanf           # read input value
 incl    4(%esp)         # increment value on the stack
 call    printf          # print out incremented value
+
 leave                   # restore old stack frame
 ret
