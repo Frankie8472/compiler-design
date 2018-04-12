328742954d35ef0cdc77de48f0d22e87032453d4
Frankie <knobelf@student.ethz.ch>
2018-04-12 22:34:18 +0200

    tampered with semantic checker

--- HW3/src/cd/frontend/semantic/SemanticChecker.java
+++ HW3/src/cd/frontend/semantic/SemanticChecker.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class SemanticChecker extends AstVisitor<Void, SymbolWrapper> {
 
-    private TypeManager typeManager;
+    private TypeManager typeManager; // FIELD!
 
     public SemanticChecker(TypeManager typeManager){
         this.typeManager = typeManager;
@@ -50,18 +50,22 @@ public class SemanticChecker extends AstVisitor<Void, SymbolWrapper> {
         return null;
     }
 
+    /**
+     * Handles the method calls. checks arguments and receiver
+     * @param ast The AST node of the method call
+     * @param arg
+     * @throws SemanticFailure if the number of given arguments does not match the number of declared arguments
+     *
+     */
     @Override
     public Void methodCall(Ast.MethodCallExpr ast, SymbolWrapper arg) {
 
         MethodSymbol methodSymbol;
         Symbol.TypeSymbol receiverTypeSymbol;
 
-        for (Expr expr : ast.argumentsWithoutReceiver()){
-            visit(expr, arg);
-        }
+        visitChildren(ast, arg);
 
         if (ast.receiver() != null) {
-            visit(ast.receiver(), arg);
             receiverTypeSymbol = ast.receiver().type;
         } else {
             receiverTypeSymbol = (TypeSymbol) arg.parentSymbol.symbol;
@@ -71,16 +75,11 @@ public class SemanticChecker extends AstVisitor<Void, SymbolWrapper> {
 
         ast.type = methodSymbol.returnType;
 
-        if (methodSymbol == null) {
-            throw new SemanticFailure(SemanticFailure.Cause.NO_SUCH_METHOD);
-        }
-
         if (methodSymbol.parameters.size() != ast.argumentsWithoutReceiver().size()) {
             throw new SemanticFailure(SemanticFailure.Cause.WRONG_NUMBER_OF_ARGUMENTS);
         }
 
         for (int i = 0; i < methodSymbol.parameters.size(); i++) {
-            visit(ast.argumentsWithoutReceiver().get(i), arg);
             TypeSymbol should_be_symbol = methodSymbol.parameters.get(i).type;
             TypeSymbol is_symbol = ast.argumentsWithoutReceiver().get(i).type;
             if (typeManager.isAssignable(should_be_symbol, is_symbol)) {
@@ -94,7 +93,7 @@ public class SemanticChecker extends AstVisitor<Void, SymbolWrapper> {
     @Override
     public Void assign(Ast.Assign ast, SymbolWrapper arg) {
         visitChildren(ast, null);
-        if (!ast.left().type.equals(ast.right().type)) {    // todo: fix subtyping
+        if (!typeManager.isAssignable(ast.left().type, ast.right().type)) {
             throw new SemanticFailure(SemanticFailure.Cause.TYPE_ERROR); // todo: choose correct cause
         }
 
