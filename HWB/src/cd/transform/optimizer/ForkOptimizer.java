67aea7519df721a7acb4ed279f190087f2465a4f
Frankie <knobelf@student.ethz.ch>
2018-05-22 14:38:06 +0200

    created the ForkOptimizer

---
+++ HWB/src/cd/transform/optimizer/ForkOptimizer.java
@@ -0,0 +1,61 @@
+package cd.transform.optimizer;
+
+import cd.ir.Ast;
+import cd.ir.AstVisitor;
+import cd.ir.BasicBlock;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class ForkOptimizer extends AstVisitor<Object, Object> {
+    private Ast.MethodDecl methodDecl;
+
+    public ForkOptimizer(Ast.MethodDecl methodDecl) {
+        this.methodDecl = methodDecl;
+    }
+
+    private void optimize() {
+        for (BasicBlock topBlock : methodDecl.cfg.allBlocks) {
+            if (topBlock.condition != null && topBlock.condition instanceof Ast.BooleanConst) {
+                Boolean cond = ((Ast.BooleanConst) topBlock.condition).value;
+                BasicBlock currBlock;
+                Set<BasicBlock> killed = new HashSet<>();
+                Set<BasicBlock> todo = new LinkedHashSet<>();
+                if (cond) { // True, kill false
+                    todo.add(topBlock.falseSuccessor());
+                    topBlock.successors.remove(topBlock.falseSuccessor());
+                    topBlock.condition = null;
+
+                    while (!todo.isEmpty()) {
+                        currBlock = todo.iterator().next();
+                        todo.remove(currBlock);
+                        killed.add(currBlock);
+                        for (BasicBlock succBlock : currBlock.successors) {
+                            if (!killed.contains(succBlock) && !topBlock.dominanceFrontier.contains(succBlock)) {
+                                todo.add(succBlock);
+                            }
+                        }
+                    }
+                } else { // False, kill true
+                    todo.add(topBlock.trueSuccessor());
+                    topBlock.successors.remove(topBlock.trueSuccessor());
+                    topBlock.condition = null;
+
+                    while (!todo.isEmpty()) {
+                        currBlock = todo.iterator().next();
+                        todo.remove(currBlock);
+                        killed.add(currBlock);
+                        for (BasicBlock succBlock : currBlock.successors) {
+                            if (!killed.contains(succBlock) && !topBlock.dominanceFrontier.contains(succBlock)) {
+                                todo.add(succBlock);
+                            }
+                        }
+                    }
+                }
+
+                methodDecl.cfg.allBlocks.removeAll(killed);
+            }
+        }
+    }
+}
